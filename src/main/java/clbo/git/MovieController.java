9d80ab2c3388ea47a632d05e7cfa5f366b3c375c
clbokea <clbo@kea.dk>
2018-09-11 09:40:39 +0200

    initial

---
+++ src/main/java/clbo/git/MovieController.java
@@ -0,0 +1,15 @@
+package clbo.git;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MovieController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+
+}
